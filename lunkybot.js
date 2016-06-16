8390a826c85302ecb7ffe855dd750d4863748c29
Chris Connett <chris@connett.net>
2016-06-16 00:21:08 -0400

    Write tests and make recent runs work.

--- lunkybot.js
+++ lunkybot.js
@@ -8,27 +8,122 @@ const twitch = require('twitch-api');
 const auth = require('./auth');
 const matcher = require('./matcher');
 
-const actions = [
-  {
-    pattern: /!(stats|wr) (.*)/,
-    reply: function(message, groups) {
-      var [_, command, arg] = groups;
-      let params = {
-        stats: {
-          url: 'http://mossranking.mooo.com/api/userlist.php',
-          fn: statsMessage
-        },
-        wr: {url: 'http://mossranking.mooo.com/api/catdef.php', fn: wrMessage}
-      };
-      get(params[command].url).then(function(json) {
-        try {
-          params[command]
-              .fn(JSON.parse(json), arg)
-              .then(text => bot.reply(message, text));
-        } catch (e) {
-          bot.reply(message, 'Something went wrong.');
+
+const recentRunsChannelId = '190522354114363392';
+
+class Lunkybot {
+  constructor(actions) {
+    this.actions = actions;
+    this.client = new discord.Client();
+  }
+
+  start() {
+    this.client.on('message', message => {
+      this.actions.forEach(action => {
+        let groups = action.pattern.exec(message.content);
+        if (groups) {
+          if (typeof action.reply === 'string') {
+            this.client.reply(message, action.reply);
+          } else {
+            action.reply(message, groups);
+          }
         }
       });
+    });
+    this._postNewRunsHandle = setInterval(() => this.fetchAndPost(), 60000);
+    return this.client.loginWithToken(auth.token);
+  }
+
+
+  stop() { clearInterval(this._postNewRunsHandle); }
+
+  fetch(url) {
+    return get(url).then(JSON.parse).catch(e => {
+      console.log(e);
+      return Promise.reject(e);
+    });
+  }
+
+  fetchAndPost() {
+    return this.fetch('http://mossranking.mooo.com/api/recent_runs.php')
+        .then(r => Promise.all(this.postNewRuns(r)));
+  }
+  postNewRuns(recentRuns) {
+    let results = [];
+    let logs = this.client.getChannelLogs(recentRunsChannelId);
+    for (let [id_user, username, cat, category, submitted, scorerun, timerun,
+              timerun_hf, world, level, flag_wr, link, comment] of recentRuns
+             .slice(1)) {
+      let content = '';
+      if (parseInt(flag_wr)) {
+        content += `${username} set a new World Record for ${category}! `;
+      } else {
+        content += `${username} completed a ${category} run! `;
+      }
+      if (parseInt(scorerun)) {
+        let score = new Intl
+                        .NumberFormat('en-US', {
+                          style: "currency",
+                          currency: "usd",
+                          minimumFractionDigits: 0
+                        })
+                        .format(parseInt(scorerun));
+        content += `The score was ${score}.`;
+      } else {
+        content += `The time was ${timerun_hf}.`;
+      }
+      logs.then(logs => {
+        let previousMessage =
+            logs.filter(message => message.content == content);
+        if (previousMessage.length == 0) {
+          results.push(this.client.sendMessage(recentRunsChannelId, content));
+        }
+      });
+    }
+    return results;
+  }
+}
+
+function statsMessage(players, arg) {
+  let lowerPlayerToId = {};
+  for (let [id, name, country, sprite, twitch] of players.slice(1)) {
+    lowerPlayerToId[name.toLowerCase()] = id;
+  }
+  let match =
+      matcher.getClosestMatch(arg.toLowerCase(), Object.keys(lowerPlayerToId));
+  return Promise.resolve(
+      `<http://mossranking.mooo.com/stats.php?id_user=${lowerPlayerToId[match]}>`);
+}
+
+function wrMessage(categories, arg) {
+  let lowerCatToId = {};
+  for (let [id, category, shortname] of categories.slice(1)) {
+    lowerCatToId[category.toLowerCase()] = id;
+  }
+  let catId = lowerCatToId[matcher.getClosestMatch(
+      arg.toLowerCase(), Object.keys(lowerCatToId))];
+  let catName = categories[catId][1];
+  return get(`http://mossranking.mooo.com/api/getwr.php?cat=${catId}`)
+      .then(json => {
+        var [spelunker, score, time] = JSON.parse(json);
+        return `The world record for ${catName} is ${time} by ${spelunker}.`;
+      })
+      .catch(() => `I don't know what the record is for ${catName}}.`);
+};
+
+const bot = new Lunkybot([
+  {
+    pattern: /!stats (\S+)/,
+    reply: function(groups) {
+      var [_, arg] = groups;
+      bot.fetch('http://mossranking.mooo.com/api/userlist.php')
+          .then(statsMessage);
+    }
+  },
+  {
+    pattern: /!wr (.*)/,
+    reply: function() {
+      bot.fetch('http://mossranking.mooo.com/api/catdef.php').then(wrMessage);
     }
   },
   {
@@ -61,55 +156,11 @@ const actions = [
     reply: '<http://mossranking.mooo.com/records.php>'
   },
   {pattern: /!cat/, reply: function() {}},
-];
+]);
 
-function statsMessage(players, arg) {
-  let lowerPlayerToId = {};
-  for (let [id, name, country, sprite, twitch] of players.slice(1)) {
-    lowerPlayerToId[name.toLowerCase()] = id;
-  }
-  let match =
-      matcher.getClosestMatch(arg.toLowerCase(), Object.keys(lowerPlayerToId));
-  return new Promise(
-      (resolve, reject) => resolve(
-          '<http://mossranking.mooo.com/stats.php?id_user=' +
-          `${lowerPlayerToId[match]}>`));
+module.exports.Lunkybot = Lunkybot;
+module.exports.bot = bot;
+
+if (require.main === module) {
+  bot.start().catch(console.log).then(() => bot.fetchAndPost());
 }
-
-function wrMessage(categories, arg) {
-  let lowerCatToId = {};
-  for (let [id, category, shortname] of categories.slice(1)) {
-    lowerCatToId[category.toLowerCase()] = id;
-  }
-  let catId = lowerCatToId[matcher.getClosestMatch(
-      arg.toLowerCase(), Object.keys(lowerCatToId))];
-  return new Promise(function(resolve, reject) {
-    let jsonPromise =
-        get(`http://mossranking.mooo.com/api/getwr.php?cat=${catId}`);
-    jsonPromise.then(function(json) {
-      let catName = categories[catId][1];
-      try {
-        var [spelunker, score, time] = JSON.parse(json);
-      } catch (e) {
-        reject(`I don't know what the record is for ${catName}}.`);
-      }
-      resolve(`The world record for ${catName} is ${time} by ${spelunker}.`);
-    });
-  });
-}
-
-const bot = new discord.Client();
-bot.on('message', function(message) {
-  actions.forEach(function(action) {
-    let groups = action.pattern.exec(message.content);
-    if (groups) {
-      if (typeof action.reply === 'string') {
-        bot.reply(message, action.reply);
-      } else {
-        action.reply(message, groups);
-      }
-    }
-  });
-});
-
-bot.loginWithToken(auth.token);
--- lunkybot_test.js
+++ lunkybot_test.js
@@ -2,12 +2,9 @@ const rewire = require('rewire');
 const lunkybot = rewire('./lunkybot');
 
 // Get unexported symbols for testing and spying.
-const spies = {
-  actions: lunkybot.__get__('actions'),
-  statsMessage: lunkybot.__get__('statsMessage'),
-  wrMessage: lunkybot.__get__('wrMessage'),
-  get: lunkybot.__get__('get'),
-};
+const statsMessage = lunkybot.__get__('statsMessage');
+const wrMessage = lunkybot.__get__('wrMessage');
+const recentRunsChannelId = lunkybot.__get__('recentRunsChannelId');
 
 let statsData = [
   'headers', [1, 'kinnijup', null, null, null],
@@ -22,14 +19,14 @@ let catData = [
 
 describe('The matching algorithm', function() {
   it(`should match 'sat' to 'saturnin55'`, function(done) {
-    spies.statsMessage(statsData, 'sat')
+    statsMessage(statsData, 'sat')
         .then(
             msg => expect(msg).toEqual(
                 '<http://mossranking.mooo.com/stats.php?id_user=2>'))
         .finally(done);
   });
   it(`should match 'kinni' to 'kinnijup'`, function(done) {
-    spies.statsMessage(statsData, 'kinni')
+    statsMessage(statsData, 'kinni')
         .then(
             msg => expect(msg).toEqual(
                 '<http://mossranking.mooo.com/stats.php?id_user=1>'))
@@ -37,33 +34,112 @@ describe('The matching algorithm', function() {
   });
 
   beforeEach(function() {
-    spyOn(spies, 'get')
+    spyOn(lunkybot.bot, 'fetch')
         .and.returnValue(
             new Promise((res, rej) => res('["Kinnijup","99716","1:39.716"]')));
   });
   it(`should match 'egg' to 'Eggplant%`, function(done) {
-    spies.wrMessage(catData, 'egg')
+    wrMessage(catData, 'egg')
         .then(msg => expect(msg).toMatch('The world record for Eggplant% is'))
         .finally(done);
   });
   it(`should match 'low' to 'Low%`, function(done) {
-    spies.wrMessage(catData, 'low')
+    wrMessage(catData, 'low')
         .then(msg => expect(msg).toMatch('The world record for Low% is'))
         .finally(done);
   });
   it(`should match 'lowhell' to 'Low% Hell`, function(done) {
-    spies.wrMessage(catData, 'lowhell')
+    wrMessage(catData, 'lowhell')
         .then(msg => expect(msg).toMatch('The world record for Low% Hell is'))
         .finally(done);
   });
   it(`should match 'lh' to 'Low% Hell`, function(done) {
-    spies.wrMessage(catData, 'lh')
+    wrMessage(catData, 'lh')
         .then(msg => expect(msg).toMatch('The world record for Low% Hell is'))
         .finally(done);
   });
   it(`should match 'no gold' to 'No Gold`, function(done) {
-    spies.wrMessage(catData, 'no gold')
+    wrMessage(catData, 'no gold')
         .then(msg => expect(msg).toMatch('The world record for No Gold is'))
         .finally(done);
+  });
+});
+
+let runsData = [
+  [
+    "id_user", "username", "cat", "category", "submitted", "scorerun",
+    "timerun", "timerun_hf", "world", "level", "flag_wr", "link", "comment"
+  ],
+  [
+    "102", "theZaxanator", "12", "All Shortcuts + Olmec", "2016-06-14 17:32:50",
+    "0", "1382096", "23:02.096", "4", "4", "0",
+    "https:\/\/www.youtube.com\/watch?v=bfK6aALjm_E&feature=youtu.be",
+    "Stupid Category + Olmec"
+  ],
+  [
+    "73", "Brutwarst", "4", "Low%", "2016-06-14 16:01:06", "0", "100419",
+    "1:40.419", "4", "4", "1", "", "fake run"
+  ],
+  [
+    "71", "krille71", "1", "Score", "2016-06-14 16:01:06", "3000000", "0", "",
+    "4", "4", "0", "", "fake run"
+  ],
+];
+
+fdescribe('Recent runs handler', function() {
+  let zaxMessage = 'theZaxanator completed a All Shortcuts + Olmec run! ' +
+      'The time was 23:02.096.';
+  let brutMessage = 'Brutwarst set a new World Record for Low%! ' +
+      'The time was 1:40.419.';
+  let krilleMessage = 'krille71 completed a Score run! ' +
+      'The score was $3,000,000.';
+
+  beforeEach(function(done) {
+    spyOn(lunkybot.bot, 'fetch');
+    lunkybot.bot.client = jasmine.createSpyObj('client', [
+      'loginWithToken', 'channels', 'sendMessage', 'on', 'reply',
+      'getChannelLogs'
+    ]);
+    lunkybot.bot.client.channels = [{name: 'recent-runs'}];
+    lunkybot.bot.client.getChannelLogs = () => Promise.resolve([]);
+    lunkybot.bot.client.loginWithToken.and.returnValue(Promise.resolve(null));
+    lunkybot.bot.start().then(lunkybot.bot.postNewRuns([])).then(done);
+    lunkybot.bot.fetch.and.returnValue(Promise.resolve(runsData));
+  });
+  it(`should post a new run`, function(done) {
+    lunkybot.bot.fetchAndPost()
+        .then(() => {
+          expect(lunkybot.bot.client.sendMessage)
+              .toHaveBeenCalledWith(recentRunsChannelId, zaxMessage);
+        })
+        .then(done);
+  });
+  it(`should say if it's a world record`, function(done) {
+    lunkybot.bot.fetchAndPost()
+        .then(() => {
+          expect(lunkybot.bot.client.sendMessage)
+              .toHaveBeenCalledWith(recentRunsChannelId, brutMessage);
+        })
+        .then(done);
+  });
+  it(`shouldn't post duplicate run`, function(done) {
+    lunkybot.bot.client.getChannelLogs = () => Promise.resolve([
+      {content: zaxMessage}, {content: brutMessage}, {content: krilleMessage}
+    ]);
+    lunkybot.bot.fetchAndPost()
+        .then(() => {
+          expect(lunkybot.bot.client.sendMessage).not.toHaveBeenCalled();
+        })
+        .then(done);
+  });
+  it(`should format score`, function(done) {
+    lunkybot.bot.client.channels[0].getChannelLogs = () =>
+        Promise.resolve([{content: zaxMessage}, {content: brutMessage}]);
+    lunkybot.bot.fetchAndPost()
+        .then(() => {
+          expect(lunkybot.bot.client.sendMessage)
+              .toHaveBeenCalledWith(recentRunsChannelId, krilleMessage);
+        })
+        .then(done);
   });
 });
