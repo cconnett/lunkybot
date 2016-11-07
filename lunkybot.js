c9d3be8e86e127860888ca05b255bec2696839d1
Chris Connett <chris@connett.net>
2016-11-06 22:19:01 -0500

    Start irc refactor.

--- lunkybot.js
+++ lunkybot.js
@@ -3,6 +3,8 @@ require('promise.prototype.finally');
 
 const discord = require('discord.js');
 const get = require('request-promise');
+const irc = require('irc');
+const sqlite3 = require('sqlite3');
 const twitch = require('twitch-api');
 
 const auth = require('./auth');
@@ -11,33 +13,113 @@ const matcher = require('./matcher');
 
 const recentRunsChannelId = '190522354114363392';
 
+// class Bot {
+//   constructor(actions) { this.actions = actions; }
+// }
+
+// class DiscordBot extends Bot {
+//   constructor(actions) {
+//     super(actions);
+//     this.client = thing;
+//   }
+// }
+
+// class IrcBot extends Bot {}
+
+
 class Lunkybot {
   constructor(actions) {
     this.actions = actions;
-    this.client = new discord.Client();
+    this.db = new sqlite3.Database('lunkybot.db');
+    this.db.run('CREATE TABLE IF NOT EXISTS ircChannels (channel TEXT)');
+    this.db.run('CREATE TABLE IF NOT EXISTS runsNotified (signature TEXT)');
+    this.db.run(
+        'CREATE INDEX IF NOT EXISTS runsSigs ON runsNotified (signature)');
+
+
+    this.discord = new discord.Client({
+      autorun: true,
+      token: auth.discordToken,
+    });
+    this.irc = new irc.Client('irc.twitch.tv', 'Lunkybot', {
+      floodProtetion: true,
+      floodProtetionDelay: 1.5,
+      secure: false,
+      debug: true,
+      password: auth.ircToken,
+      autoConnect: true,
+    });
+
+    this._loginHandle = null;
+    this._postNewRunsHandle = null;
   }
 
   start() {
-    this.client.on('message', message => {
+    this.discord.on('message', (user, userId, channelId, message, event) => {
       this.actions.forEach(action => {
-        let groups = action.pattern.exec(message.content);
+        let groups = action.pattern.exec(message);
         if (groups) {
           if (typeof action.reply === 'string') {
-            this.client.reply(message, action.reply);
+            this.discord.sendMessage({
+              to: channelId,
+              message: action.reply,
+            });
           } else {
-            action.reply(groups).then(
-                answer => this.client.reply(message, answer));
+            action.reply(groups).then(answer => this.discord.sendMessage({
+              to: channelId,
+              message: answer,
+            }));
           }
         }
       });
     });
-    this._postNewRunsHandle =
-        setInterval(() => this.fetchAndPost(), 1000 * 60 * 15);
-    return this.client.loginWithToken(auth.token);
+    this.irc.addListener('message', (from, to, message) => {
+      this.actions.forEach(action => {
+        let groups = action.pattern.exec(message);
+        if (groups) {
+          if (typeof action.reply === 'string') {
+            this.irc.say(to, action.reply);
+          } else {
+            action.reply(groups).then(answer => this.irc.say(to, answer));
+          }
+        }
+      });
+    });
+    this.irc.addListener('pm', (from, message) => {
+      if (message == 'join') {
+        this.db.run(
+            'INSERT OR REPLACE INTO irc VALUES (?)', '#' + from, error => {
+              if (!error) {
+                self.irc.say(from, "OK, got it. To get rid of me, say 'part'.");
+              } else {
+                self.irc.say(from, 'I had an error: ' + error);
+              }
+            });
+      } else if (message == 'part') {
+        let self = this;
+        this.db.run('DELETE FROM irc WHERE channel = ?', '#' + from, error => {
+          if (!error) {
+            self.irc.say(from, "OK, I'm out.");
+          } else {
+            self.irc.say(from, 'I had an error: ' + error);
+          }
+        });
+      }
+    });
   }
 
+  startLoginService() {
+    this._postNewRunsHandle =
+        setInterval(() => this.fetchAndPost(), 1000 * 60 * 15);
+    this._loginHandle =
+        setInterval(() => this.discord.login(auth.token), 1000 * 60 * 5);
+    return this.discord.login(auth.token);
+  }
 
-  stop() { clearInterval(this._postNewRunsHandle); }
+  stop() {
+    clearInterval(this._postNewRunsHandle);
+    clearInterval(this._loginHandle);
+  }
 
   fetch(url) {
     return get(url).then(JSON.parse).catch(e => {
@@ -79,7 +161,7 @@ class Lunkybot {
   }
   postNewRuns(recentRuns) {
     let results = [];
-    let logs = this.client.getChannelLogs(recentRunsChannelId);
+    let logs = this.discord.getChannelLogs(recentRunsChannelId);
     for (let [id_user, username, cat, category, submitted, scorerun, timerun,
               timerun_hf, world, level, flag_wr, link, comment] of recentRuns
              .slice(1)) {
@@ -111,7 +193,7 @@ class Lunkybot {
                 0);
         if (previousMessage.length == 0 &&
             Date.parse(submitted) > smallestTimestamp) {
-          results.push(this.client.sendMessage(recentRunsChannelId, content));
+          results.push(this.discord.sendMessage(recentRunsChannelId, content));
         }
       });
     }
